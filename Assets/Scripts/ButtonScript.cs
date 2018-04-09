9cc4f4a2d96d9f8488ed05c6437367a3b3b19334
mashiro11 <thiagoaas1990@gmail.com>
2018-04-09 20:29:32 -0300

    Segura a bomba

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonScript : MonoBehaviour {
+
+    //Button[] btn;
+    private int selectedIndex = 0;
+    public GameObject selector;
+	// Use this for initialization
+	void Start () {
+        //btn = GetComponents<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectedIndex++;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectedIndex--;
+        }
+        selector.gameObject.transform.position = btn[selectedIndex].transform.position;
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            //Chama o click
+        }
+    }
+}
